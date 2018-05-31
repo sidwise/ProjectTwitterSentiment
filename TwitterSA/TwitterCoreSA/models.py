419cce8ec4247dc4dc821d57121de977111e3a66
Sid Ahmed <sid_wise@hotmail.com>
2018-05-31 19:25:38 +0100

    Models for neo4j wihtin app

--- TwitterSA/TwitterCoreSA/models.py
+++ TwitterSA/TwitterCoreSA/models.py
@@ -1,6 +1,8 @@
 # -*- coding: utf-8 -*-
 from __future__ import unicode_literals
 from django.db import models
+from django_neomodel import DjangoNode
+from neomodel import StructuredNode, StringProperty, DateProperty, IntegerProperty, FloatProperty, Relationship
 from . import twitter_search
 # Create your models here.
 
@@ -18,6 +20,52 @@ class Search(models.Model):
 
 
 class TweetSAAnalytics(models.Model):
+    search = models.ForeignKey(Search, on_delete=models.CASCADE)
     title = models.CharField(name="Titre", max_length=40)
     number_positive_tweets = models.IntegerField(name="Positive Tweets")
     number_negative_tweets = models.IntegerField(name="Negative Tweets")
+
+
+class Source(StructuredNode):
+    name = StringProperty()
+
+
+class Tweet(StructuredNode):
+
+    id_str = IntegerProperty()
+    text = StringProperty()
+    retweet_count = IntegerProperty()
+    favourites_count = IntegerProperty()
+    created_at = StringProperty()
+    lemm = StringProperty()
+    valence = FloatProperty()
+    arousal = FloatProperty()
+    emotion = StringProperty()
+
+    retweets = Relationship('Tweet', 'retweets')
+    reply_to = Relationship('Tweet', 'reply_to')
+    contains = Relationship('Url', 'contains')
+    mentions = Relationship('User', 'mentions')
+    using = Relationship('Source', 'using')
+
+
+class User(StructuredNode):
+
+    name = StringProperty()
+    screen_name = StringProperty()
+    location = StringProperty()
+    lang = StringProperty()
+    favourites_count = IntegerProperty()
+    followers_count = IntegerProperty()
+    friends_count = IntegerProperty()
+    description = StringProperty()
+
+    posts = Relationship('Tweet', 'posts')
+
+
+class Hashtag(StructuredNode):
+    name = StringProperty()
+
+
+class Url(StructuredNode):
+    tweet_url = StringProperty()
