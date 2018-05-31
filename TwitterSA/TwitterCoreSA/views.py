fa57690bc4e85624c2edbd305fed91503e4df2ef
Sid Ahmed <sid_wise@hotmail.com>
2018-05-31 19:26:07 +0100

    creating views and getting information to home page

--- TwitterSA/TwitterCoreSA/views.py
+++ TwitterSA/TwitterCoreSA/views.py
@@ -1,8 +1,9 @@
 # Create your views here.
 # Create your views here.
 from django.template import RequestContext
-from django.shortcuts import render_to_response
+from django.shortcuts import render_to_response, render
 from . import twitter_search
+from .models import Tweet
 import logging
 
 logger = logging.getLogger(__name__)
@@ -17,21 +18,23 @@ def index(request):
     # Note the key boldmessage is the same as {{ boldmessage }} in the
     # template!
     # context_dict = {'boldmessage': "I am bold font from the context"}
-
+    context = {'tweets': Tweet.nodes.all()}
     # Return a rendered response to send to the client.
     # We make use of the shortcut function to make our lives easier.
     # Note that the first parameter is the template we wish to use.
-    return render_to_response(
-        'TwitterCoreSA/index.html')
+    return render(request,
+                  'TwitterCoreSA/index.html', context)
 
 
 def search(request):
     # Request the context of the request.
     # The context contains information such as the client's machine details,
     # for example.
-    if(request.GET.get('submit_but')):
-        twitter_search.TwitterSearch(
-            str(request.GET.get('tag')), str(request.GET.get('nbr')))
+    # tag = request.GET.get('tag')
+    # nbr = request.GET.get('nbr')
+    # if(request.GET.get('submit_but')):
+    #     twitter_search.TwitterSearch(
+    #         str(), str())
     # Construct a dictionary to pass to the template engine as its context.
     # Note the key boldmessage is the same as {{ boldmessage }} in the
     # template!
@@ -52,5 +55,5 @@ def submit(request):
     # context = RequestContext(request)
     twitter_search.TwitterSearch(
         str(request.POST.get('tag')), str(request.POST.get('nbr')))
-    return render_to_response(
-        'TwitterCoreSA/recherche.html')
+    # return render(
+    #     'TwitterCoreSA/recherche.html')
