45a5cf56913e92d3ea00f9166f6048e484aae2a6
Sid Ahmed <sid_wise@hotmail.com>
2018-06-07 11:44:19 +0100

    Dashboard pie and word cloud

--- TwitterSA/TwitterCoreSA/views.py
+++ TwitterSA/TwitterCoreSA/views.py
@@ -1,9 +1,13 @@
-from django.template import RequestContext
-from django.shortcuts import render_to_response, render
+# from django.template import RequestContext
+from django.shortcuts import render
 from .models import Tweet
 import logging
 from TwitterCoreSA.tasks import TwitterSearch
 import json
+
+from matplotlib.pyplot import figure, title, bar, pie
+import numpy as np
+import mpld3
 # from wordcloud import WordCloud, STOPWORDS
 # import matplotlib.pyplot as plt
 # import io
@@ -17,62 +21,49 @@ logger = logging.getLogger(__name__)
 def index(request):
     # Request the context of the request.
     # The context contains information such as the client's machine details,
-    # for example.
 
     # Construct a dictionary to pass to the template engine as its context.
     # Note the key boldmessage is the same as {{ boldmessage }} in the
     # template!
-    # context_dict = {'boldmessage': "I am bold font from the context"}
 
     tweets = Tweet.nodes.all()
     lemms = []
-    # define maximum rank as weight for CSS tags
-    MAX_WEIGHT = 5
     # define number of keywords to display in cloud
-    NUMBER_OF_KEYWORDS = 200
+    NUMBER_OF_KEYWORDS = 500
+    valence = []
     for t in tweets:
         if t.lemm:
             lemms += t.lemm.split()
+        if t.valence:
+            valence.append(t.valence)
+
+    positive = sum(1 for i in valence if i > 5) * 100 / len(valence)
+    negative = sum(1 for i in valence if i < 5) * 100 / len(valence)
+    logger.error(positive)
+    logger.error(negative)
     lemms = filter(lambda a: a != 'USER_MENTION' and a != 'URL', lemms)
     word = Counter(lemms)
-    # wordcloud = WordCloud(width=1000, height=500).generate(" ".join(lemms))
     sorted_keywords = sorted(
         word.items(), key=operator.itemgetter(1), reverse=True)
     sorted_keywords = sorted_keywords[0:NUMBER_OF_KEYWORDS]
     word = dict(sorted_keywords)
-
-    # logger.info(word)
-    # keywords = '['
     keywords = []
     for key, value in word.iteritems():
-        # keywords += "{text: '" + key + "', " + "size: " + str(value) + "}"
-        keywords.append({'text': key, 'size': value})
-    #     i += 1
-    #     if i < NUMBER_OF_KEYWORDS:
-    #         keywords += ","
-    #     # keywords.append({'text': key, 'size': value})
-    # keywords += "]"
-    # keywords = json.dumps(keywords)
+        keywords.append({'text': key.decode('utf-8'), 'size': value})
+    mpl_figure = figure(1, figsize=(6, 6))
+    xvalues = range(2)  # the x locations for the groups
+    yvalues = [positive, negative]
+    width = 0.5  # the width of the bars
+    title(u'Positive and negative Emotions')
+    # bar(xvalues, yvalues, width)
+    labels = 'Positive', 'Negative'
+    pie(yvalues,  labels=labels, autopct='%1.1f%%',
+        shadow=True, startangle=90)
 
-    # min_count = max_count = keywords[0]['count']
-    # for keyword in keywords:
-    #     if keyword['count'] < min_count:
-    #         min_count = keyword['count']
-    #     if max_count < keyword['count']:
-    #         max_count = keyword['count']
-    # range = float(max_count - min_count)
-    # if range == 0.0:
-    #     range = 1.0
-    # for keyword in keywords:
-    #     # if keyword['count']
-    #     keyword['weight'] = int(
-    #         MAX_WEIGHT * (keyword['count'] - min_count) / range)
-    context = {'tweets': json.dumps(keywords, ensure_ascii=False)}
-    logger.info(context)
+    fig_html = mpld3.fig_to_html(mpl_figure)
+    context = {'tweets': json.dumps(
+        keywords, ensure_ascii=False), 'figure': fig_html}
 
-    # figure = io.BytesIO()
-    # plt.savefig(figure, format="png")
-    # logger.info(lemms)
     # content_file = ImageFile(figure)
     # Return a rendered response to send to the client.
     # We make use of the shortcut function to make our lives easier.
